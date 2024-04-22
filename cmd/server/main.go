5c72c000bc120b368738b2590025ae6214d5493b
Sam Myres <sam@myres.dev>
2024-04-22 10:33:34 -0500

    add migration sequence, sql insert

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"strings"
 
 	"github.com/samiam2013/go-pi-pmu/measurement/protobuf"
 	"google.golang.org/protobuf/proto"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	connectStr := "postgresql://grafana:grafana@localhost/grafana?sslmode=disable"
+	db, err := sql.Open("postgres", connectStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := migrateSchema(db); err != nil {
+		log.Fatal(err)
+	}
+
 	srv := &http.Server{
 		Addr: ":8080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -27,11 +43,54 @@ func main() {
 			if _, err := w.Write([]byte(series.String())); err != nil {
 				panic(err)
 			}
-			// log.Print(series.String())
+
+			// build an insert for this data
+			queryPrefix := "INSERT INTO pmu(voltage, current, epoch_nano) VALUES"
+			var sb strings.Builder
+			sb.WriteString(queryPrefix)
+			for _, measurement := range series.Measurements {
+				sb.WriteString(fmt.Sprintf("(%d, %d, %d),",
+					measurement.Voltage, measurement.Current, measurement.Epochnano))
+			}
+			if _, err := db.Exec(strings.TrimRight(sb.String(), ",")); err != nil {
+				log.Printf("failed to insert data: %v", err)
+			}
+
 		}),
 	}
 	if err := srv.ListenAndServe(); err != nil {
 		panic(err)
 	}
 
+}
+
+func migrateSchema(db *sql.DB) error {
+	query := "SELECT version FROM migration ORDER BY version DESC LIMIT 1"
+	row := db.QueryRow(query)
+	var version int64
+	if err := row.Scan(&version); err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return fmt.Errorf("failed querying for migration version: %w", err)
+	}
+	log.Printf("loaded migration: %d", version)
+	migrations := map[int64]string{
+		1: "CREATE TABLE IF NOT EXISTS pmu ( voltage INT, current INT, epoch_nano BIGINT)",
+		2: "CREATE INDEX IF NOT EXISTS idx_epoch_nano ON pmu (epoch_nano)",
+	}
+	for i := int64(1); true; i++ {
+		v, ok := migrations[i]
+		if !ok {
+			break
+		}
+		if version >= i {
+			break
+		}
+		if _, err := db.Exec(v); err != nil {
+			return fmt.Errorf("failed migration %s, %w", v, err)
+		}
+		upMigVer := "INSERT INTO migration(version) VALUES($1)"
+		if _, err := db.Exec(upMigVer, i); err != nil {
+			return fmt.Errorf("failed updating migration version: %v", err)
+		}
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/samiam2013/go-pi-pmu
 go 1.22.2
 
 require (
+	github.com/lib/pq v1.10.9
 	github.com/spf13/pflag v1.0.5
 	golang.org/x/time v0.5.0
 	google.golang.org/protobuf v1.33.0
